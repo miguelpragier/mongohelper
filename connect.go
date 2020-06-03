cd31a1edb90b2bdbc47433f404895e90c8d2eec7
miguel <miguelpragier@gmail.com>
2020-06-03 15:03:24 -0300

    Some formatting and reengineering of connection routines

--- connect.go
+++ connect.go
@@ -8,65 +8,69 @@ import (
 	"time"
 )
 
+func (l Link) defineLink(opts *options.ClientOptions) error {
+	ctx, cancel := context.WithTimeout(context.Background(), l.connTimeout())
+
+	defer cancel()
+
+	c, err := mongo.Connect(ctx, opts)
+
+	if err != nil {
+		l.log("link.defineLink.mongo.Connect", err.Error())
+
+		return err
+	}
+
+	l.client = c
+
+	return nil
+}
+
 func (l Link) ping() error {
 	ctx, cancel := context.WithTimeout(context.Background(), l.connTimeout())
 
 	defer cancel()
 
-	return l.client.Ping(ctx, readpref.Primary())
+	err := l.client.Ping(ctx, readpref.Primary())
+
+	if err != nil {
+		l.log("link.ping", err.Error())
+	}
+
+	return err
 }
 
 // connect tries to conect database using the given options
 func (l *Link) connect() error {
-	var ctx context.Context
+	opts := options.Client().ApplyURI(l.connectionString())
+	opts.SetConnectTimeout(l.connTimeout())
+	opts.SetMaxConnIdleTime(8 * time.Hour)
+	opts.SetSocketTimeout(l.execTimeout())
+	opts.SetMinPoolSize(10)
+	opts.SetAppName(l.appName())
 
-	// Context with timeout can't be used in loops, because they expire before the loop complete its job
-	if l.insistOnFail() {
-		ctx = context.Background()
-	} else {
-		timeout := time.Duration(l.options.connTimeoutSeconds) * time.Second
-
-		_ctx, cancel := context.WithTimeout(context.Background(), timeout)
-
-		ctx = _ctx
-
-		defer cancel()
+	// It's not possible to restore from errors in options validation
+	if err := l.defineLink(opts); err != nil {
+		return err
 	}
 
 	for {
-		var err error
-
-		opts := options.Client().ApplyURI(l.connectionString())
-		opts.SetConnectTimeout(l.connTimeout())
-		opts.SetMaxConnIdleTime(8 * time.Hour)
-		opts.SetSocketTimeout(l.execTimeout())
-		opts.SetMinPoolSize(10)
-		opts.SetAppName(l.appName())
-
-		l.client, err = mongo.Connect(ctx, opts)
+		err := l.ping()
 
 		if err != nil {
-			l.log("mongo.Connect", err.Error())
-		}
+			l.log("link.mongo.Ping", err.Error())
 
-		err = l.client.Ping(context.Background(), readpref.Primary())
-
-		if err != nil {
-			l.log("mongo.Ping", err.Error())
-		} else {
-			l.notifyConnection()
-
-			return nil
-		}
-
-		if l.insistOnFail() {
-			if l.canInsist() {
+			if l.insistOnFail() && l.canInsist() {
 				l.wait()
 				l.increment()
 				continue
+			} else {
+				return err
 			}
 		}
 
-		return err
+		l.notifyConnection()
+
+		return nil
 	}
 }
